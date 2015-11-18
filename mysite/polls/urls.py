cfcb06161a2e67d31e8e8caf21ba5423cb046922
tkingless <tkingless@github.com>
2015-11-18 06:50:33 +0000

    add few functions, urls and views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    # tkkA: refer to views.py, detail is the function name correspondingly
+    # tkkA: so question_id is the group name/input parameter id stored when grep the result
+    #      see views.py, % question_id, it is where the function input param come from
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
