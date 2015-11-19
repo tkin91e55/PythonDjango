80da11985c16e0e7cf1090b165743a22b4d018cc
tkingless <tkingless@github.com>
2015-11-19 07:27:10 +0000

    add voting functions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,9 @@
 #tkkA: still need HttpResponse if having stub methods for detail,results and vote
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -16,8 +16,26 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # tkkA: helps avoid having to hardcode a URL in the view function. 
+        #      It is given the name of the view that we want to pass control
+        #      to and the variable portion of the URL pattern that points to that view
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
