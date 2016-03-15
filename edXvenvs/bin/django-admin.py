6bda82a9d3aaac948187f5a825485cde01f77508
tkingless <t.kingless@yahoo.com>
2016-03-15 03:34:09 +0000

    install django 1.4.21

---
+++ edXvenvs/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vagrant/Github/DjangoNginxGunicorn/PythonDjango/edXvenvs/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
