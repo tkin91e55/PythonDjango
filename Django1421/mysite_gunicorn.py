d63922becbf1380f6857adb0745e01a20842fad8
tkingless <t.kingless@yahoo.com>
2016-03-24 08:53:07 +0000

    hide gunicorn behind nginx

--- Django1421/mysite_gunicorn.py
+++ Django1421/mysite_gunicorn.py
@@ -3,8 +3,7 @@ import multiprocessing
 #preload_app is gunicorn settings,http://docs.gunicorn.org/en/stable/settings.html#server-mechanics
 preload_app = True
 timeout = 300
-#bind = "127.0.0.1:8080"
-bind = "0.0.0.0:8080"
+bind = "127.0.0.1:8080"
 pythonpath = "/home/vagrant/Github/DjangoNginxGunicorn/PythonDjango/Django1421/mysite"
 
 workers = (multiprocessing.cpu_count()-1) * 4 + 4
